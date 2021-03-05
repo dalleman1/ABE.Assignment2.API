7343807fce8f720b8833a7225c7ce599ff7e7bb1
Morten Dalsgaard <monglade.dalsgaard@gmail.com>
2021-03-05 11:38:40 +0100

    adding schemas

---
+++ ABE.Assignment2.DomainLogic/DTO's/GraphQLQueryDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ABE.Assignment2.DomainLogic.DTO_s
+{
+    public class GraphQLQueryDTO
+    {
+        public string OperationName { get; set; }
+        public string NamedQuery { get; set; }
+        public string Query { get; set; }
+        public string Variables { get; set; }
+    }
+}
